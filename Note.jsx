24648ed141470a22d985ae54266a5c556548c32b
Alampally Manish <82049208+manishguptha-001@users.noreply.github.com>
2021-06-05 08:31:19 -0800

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function NOTE() {
+  return (
+   <div class="note">
+     <h1>Javascript and React js</h1>
+     <p>This was an amazing bootcamp from ShapeAI covered from scratch 
+       including javascript and react.js(only basics)
+     </p>
+   </div>
+  );
+}
+
+export default NOTE;
